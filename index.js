84ccda091d74c547dc83a5dccac5b1b797013f9c
Ben_G <ben@sveltestudios.com>
2022-09-20 12:55:00 +0930

    added it to the menu

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 import Tag from './src/lib/Editors/Tag/index.vue';
 import TagFilter from './src/lib/Editors/TagFilter/index.vue';
 import Settings from './src/lib/SiteSetting/index.vue';
+import TagIcon from './src/lib/TagIcon.vue';
 
 export default {
   name: 'Tags',
@@ -19,6 +20,23 @@ export default {
       name: 'tagFilter',
       label: 'Tag Filter',
       component: TagFilter,
+    },
+  ],
+  menuItems: [
+    {
+      key: 'tag',
+      label: 'Change Page Tags',
+      icon: TagIcon,
+      action: ({ clearSelectedContent, clearSelectedComponent, openInSideBar, selectComponent, getPage }) => {
+        clearSelectedContent();
+        clearSelectedComponent();
+        selectComponent({
+          value: { value: getPage(), property: 'tags' },
+          el: document.getElementsByTagName('body')[0],
+        });
+        return openInSideBar('tag');
+      },
+      order: 410,
     },
   ],
 };
